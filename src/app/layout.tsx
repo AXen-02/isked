995189eceea62cdff6fefdabbf633c2bea105200
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-08-09 23:39:47 +0800

    save: added theme provider, tailwindindicator, favicons, og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,53 @@
+import { siteConfig } from "@/config/site";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/providers";
+import { TailwindIndicator } from "@/components/tailwind-indicator";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Isked",
-  description: "Automated Class Scheduler",
+  title: { default: siteConfig.name, template: `%s - ${siteConfig.name}` },
+  description: siteConfig.description,
+  keywords: [
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+    "Class Schedule",
+  ],
+  authors: [
+    {
+      name: "axen",
+      url: "https://imapp-web.vercel.app",
+    },
+  ],
+  creator: "axen",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  icons: {
+    icon: "/app/favicon.ico",
+    shortcut: "/app/favicon-16x16.png",
+    apple: "/app/apple-touch-icon.png",
+  },
+  // manifest: `${siteConfig.url}/site.webmanifest`,
 };
 
 export default function RootLayout({
@@ -15,8 +56,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.className
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="relative flex min-h-screen flex-col">
+            <div className="flex-1">{children}</div>
+          </div>
+          <TailwindIndicator />
+        </ThemeProvider>
+        {/* 
+          <ThemeSwitcher />
+          <Analytics />
+          <NewYorkToaster />
+          <DefaultToaster />
+        */}
+      </body>
     </html>
   );
 }
---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+    name: "isked",
+    url: "https://isked.vercel.app",
+    ogImage: "https://ui.shadcn.com/og.jpg", // TODO: Replace with own image
+    description:
+      "Automated class scheduling companion.",
+    links: {
+      github: "https://github.com/AXen-02/isked.git",
+    },
+  }
+  
+  export type SiteConfig = typeof siteConfig
