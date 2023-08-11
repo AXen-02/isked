f220ce8d9458d48a3934ea62a3309416eef311d6
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-08-12 02:40:46 +0800

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-import Image from "next/image";
+"use client";
 import AlertDemo from "@/components/AlertDemo";
+import { Variants, motion } from "framer-motion";
+
+const pageTransition: Variants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+};
+
+const transition = { duration: 0.5 };
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <motion.main
+      className="flex min-h-screen flex-col items-center justify-between p-24"
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={transition}
+      variants={pageTransition}
+    >
       landing page here
       <AlertDemo title="Heya!" description="Welcome to Isked." />
-    </main>
+    </motion.main>
   );
 }
