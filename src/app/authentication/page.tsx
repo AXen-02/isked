3cefa56610f758d0586292bb028a57f37f5e3fe4
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-08-13 14:06:05 +0800

    save

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -47,7 +47,7 @@ const AuthenticationPage: FC<AuthenticationPageProps> = ({}) => {
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2">
-              <h1 className="text-2xl font-semibold tracking-tight">
+              <h1 className="text-4xl font-semibold tracking-tight">
                 Join isked.
               </h1>
               <p className="text-sm text-justify text-muted-foreground">
