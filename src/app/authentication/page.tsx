da12307e7b7b1e3d8c65e196dc4dd587194d327f
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-08-13 13:34:23 +0800

    save

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -15,13 +15,13 @@ interface AuthenticationPageProps {}
 const AuthenticationPage: FC<AuthenticationPageProps> = ({}) => {
   return (
     <>
-      {/* <div className="relative flex h-full flex-col bg-muted p-10 text-white dark:border-r lg:hidden">
+      <div className="relative flex-col bg-muted p-10 text-white dark:border-r lg:hidden">
         <div className="absolute inset-0 bg-slate-900" />
         <div className="relative z-20 flex items-center text-2xl font-medium">
           <Icons.logo className="mr-2 h-6 w-6" />
           Isked
         </div>
-      </div> */}
+      </div>
       <div className="container grid h-[85vh] flex-col items-center justify-center lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
           <div className="absolute inset-0 bg-slate-900" />
