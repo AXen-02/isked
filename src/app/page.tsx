fad4cdffc430858475dbd2d7ce6cc57b13d34108
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-08-13 08:43:26 +0800

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-"use client";
-import AlertDemo from "@/components/AlertDemo";
-import { Variants, motion } from "framer-motion";
-
-const pageTransition: Variants = {
-  initial: { opacity: 0 },
-  animate: { opacity: 1 },
-  exit: { opacity: 0 },
-};
-
-const transition = { duration: 0.5 };
+import LandingPage from "@/components/LandingPage";
 
 export default function Home() {
   return (
-    <motion.main
-      className="flex min-h-screen flex-col items-center justify-between p-24"
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      transition={transition}
-      variants={pageTransition}
-    >
-      landing page here
-      <AlertDemo title="Heya!" description="Welcome to Isked." />
-    </motion.main>
+    <div>
+      <LandingPage />
+    </div>
   );
 }
