6241ed58fc47261e962cef62739e88da6ffc6186
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-08-22 17:55:26 +0800

    save

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,83 +1,83 @@
 import { NextAuthOptions, getServerSession } from "next-auth";
-import {PrismaAdapter} from '@next-auth/prisma-adapter'
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { db } from "./db";
-import GoogleProvider from "next-auth/providers/google"
+import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
-import {nanoid} from 'nanoid'
+import { nanoid } from "nanoid";
 
 export const authOptions: NextAuthOptions = {
-    adapter: PrismaAdapter(db),
-    session: {
-        strategy:'jwt'
-    },
-    pages:{
-        signIn: '/sign-in'
-    },
-    providers: [
-        GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+  adapter: PrismaAdapter(db),
+  session: {
+    strategy: "jwt",
+    maxAge: 60, // * 60 * 24 * 1, // Set maxAge to 1 day (in seconds)
+  },
+  pages: {
+    signIn: "/sign-in",
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-        GitHubProvider({
-        clientId: process.env.GITHUB_CLIENT_ID!,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET!
-    })],
-    callbacks: {
-        async session({token, session}) {
-            if(token){
-                session.user.id = token.id
-                session.user.name = token.name
-                session.user.email = token.email
-                session.user.image = token.picture
-                session.user.username = token.username
-            }
-            return session
-        },
-
-        async jwt ({token, user}) {
-            
-            const dbUser = await db.user.findFirst({
-                where: {
-                    email: token.email,
-                }
-            })
-
-            // TODO: 
-            // const dbAccount = await db.account.findFirst({
-            //     where: {
-            //         userId: token.id
-            //     }
-            // })
-
-            if(!dbUser) {
-                token.id = user!.id
-                return token
-            }
-
-            if(!dbUser.username) {
-                await db.user.update({
-                    where: {
-                        id: dbUser.id,
-                    },
-                    data: {
-                        username: nanoid(10)
-                    }
-                })
-            }
-
-            return {
-                id: dbUser.id,
-                name: dbUser.name,
-                email: dbUser.email,
-                picture: dbUser.image,
-                username: dbUser.username,
-            }
-        },
-        redirect () {
-            return '/'
-        }
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    async session({ token, session }) {
+      if (token) {
+        session.user.id = token.id;
+        session.user.name = token.name;
+        session.user.email = token.email;
+        session.user.image = token.picture;
+        session.user.username = token.username;
+      }
+      return session;
     },
-}
 
+    async jwt({ token, user }) {
+      const dbUser = await db.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
 
-export const getAuthSession = () => getServerSession(authOptions)
+      // TODO:
+      // const dbAccount = await db.account.findFirst({
+      //     where: {
+      //         userId: token.id
+      //     }
+      // })
+
+      if (!dbUser) {
+        token.id = user!.id;
+        return token;
+      }
+
+      if (!dbUser.username) {
+        await db.user.update({
+          where: {
+            id: dbUser.id,
+          },
+          data: {
+            username: nanoid(10),
+          },
+        });
+      }
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+        username: dbUser.username,
+      };
+    },
+    redirect() {
+      return "/";
+    },
+  },
+};
+
+export const getAuthSession = () => getServerSession(authOptions);
