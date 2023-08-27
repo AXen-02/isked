dcdcea7772b4f4558f12881c5b2b8ebc9fbbb200
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-08-27 21:22:31 +0800

    save

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -62,13 +62,7 @@ export const authOptions: NextAuthOptions = {
         const defaultBio = "Hi! I'm new to Isked.";
         const defaultUrls = [
           {
-            value: `1isked.vercel.app/${randUName}`,
-          },
-          {
-            value: `2isked.vercel.app/${randUName}`,
-          },
-          {
-            value: `3isked.vercel.app/${randUName}`,
+            value: `isked.vercel.app/${randUName}`,
           },
         ];
         await db.user.update({
