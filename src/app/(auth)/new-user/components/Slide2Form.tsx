fdd25f824e24b9aed5fd475e44383188bb3ac0c1
AXen-02 <50175472+AXen-02@users.noreply.github.com>
2023-10-02 06:05:23 +0800

    save

--- src/app/(auth)/new-user/components/Slide2Form.tsx
+++ src/app/(auth)/new-user/components/Slide2Form.tsx
@@ -1,4 +1,9 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { FC } from "react";
+import { availableSchools } from "../data/schools";
+import { SchoolCard } from "./school-card";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Scrollbar } from "swiper/modules";
 
 interface Slide2FormProps {
   user: {
@@ -9,7 +14,32 @@ interface Slide2FormProps {
 }
 
 const Slide2Form: FC<Slide2FormProps> = ({ user }) => {
-  return <div>Slide2Form</div>;
+  return (
+    <div>
+      <div className="flex">
+        <Swiper
+          slidesPerView={3}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Scrollbar]}
+          className="flex w-80"
+        >
+          {availableSchools.map((school) => (
+            <SwiperSlide key={school.id}>
+              <SchoolCard
+                school={school}
+                className="w-[150px]"
+                aspectRatio="square"
+                width={150}
+                height={150}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
 };
 
 export default Slide2Form;
---
+++ src/app/(auth)/new-user/data/schools.ts
@@ -0,0 +1,82 @@
+export interface School {
+  id: string;
+  name: string;
+  location: string;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  image: string;
+}
+
+export const availableSchools: School[] = [
+  {
+    id: "1",
+    name: "Cavite State University - Indang",
+    location: "Indang Cavite",
+    description: "Main campus",
+    createdAt: Date.now().toLocaleString(),
+    updatedAt: Date.now().toLocaleString(),
+    image:
+      "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
+  },
+  {
+    id: "2",
+    name: "Cavite State University - Carmona",
+    location: "Carmona Cavite",
+    description: "Carmona campus",
+    createdAt: Date.now().toLocaleString(),
+    updatedAt: Date.now().toLocaleString(),
+    image:
+      "https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80",
+  },
+  {
+    id: "3",
+    name: "Batangas State University - Alangilan",
+    location: "Alangilan Batangas",
+    description: "Alangilan campus",
+    createdAt: Date.now().toLocaleString(),
+    updatedAt: Date.now().toLocaleString(),
+    image:
+      "https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80",
+  },
+  {
+    id: "4",
+    name: "Batangas State University - Alangilan",
+    location: "Alangilan Batangas",
+    description: "Alangilan campus",
+    createdAt: Date.now().toLocaleString(),
+    updatedAt: Date.now().toLocaleString(),
+    image:
+      "https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80",
+  },
+  {
+    id: "5",
+    name: "Batangas State University - Alangilan",
+    location: "Alangilan Batangas",
+    description: "Alangilan campus",
+    createdAt: Date.now().toLocaleString(),
+    updatedAt: Date.now().toLocaleString(),
+    image:
+      "https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80",
+  },
+  {
+    id: "6",
+    name: "Batangas State University - Alangilan",
+    location: "Alangilan Batangas",
+    description: "Alangilan campus",
+    createdAt: Date.now().toLocaleString(),
+    updatedAt: Date.now().toLocaleString(),
+    image:
+      "https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80",
+  },
+  {
+    id: "7",
+    name: "Batangas State University - Alangilan",
+    location: "Alangilan Batangas",
+    description: "Alangilan campus",
+    createdAt: Date.now().toLocaleString(),
+    updatedAt: Date.now().toLocaleString(),
+    image:
+      "https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80",
+  },
+];
